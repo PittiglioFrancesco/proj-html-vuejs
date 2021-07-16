ebd0f28e50784c0e738501dab4b7281b2b3907e9
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-16 12:39:33 +0200

    add: prima parte header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Header />
-    <Main />
-    <Footer />
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
@@ -29,8 +29,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+    <div class="row justify-content-between align-items-center line1">
+        <div class="col"><span>We have a 95% Successful Pass Rate</span></div>
+        <div class="left-text col"><i class="fas fa-phone-alt"></i><span>Give us a call to book your tuition! 1-800-555-555</span></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+// Header prima parte
+div{
+    margin: 0;
+}
+
+.line1{
+    background-color: #353637;
+    color: #929AA3;
+    height: 50px;
+    width: 100%;
+    padding: 0 10%;
+}
+
+.left-text{
+    text-align: right;
+}
+
+</style>
